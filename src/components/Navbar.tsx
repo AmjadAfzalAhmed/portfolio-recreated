577f1ef741b8b35d714527b0c2737afc13dcc099
AmjadAfzalAhmed <abhutto2006@gmail.com>
2024-11-11 16:51:32 +0500

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className="bg-[#fff] fixed top-0 left-0 right-0 z-1000 flex items-center justify-around p-[1.7rem] h-[5rem] shadow-md">
+    <header className="bg-[#fff] fixed top-0 left-0 right-0 z-1000 flex items-center justify-around p-[1.7rem] h-[4rem] shadow-md">
       <h1 className="text-2xl text-[#0e2431] font-extrabold transition ease-in duration-400  hover:text-[#fc8c05] pl-8">AmjadCreations</h1>
       <ul className="flex justify-center items-center pr-2xl ">
         <li className="text-xl text-[#0e2431] font-semibold transition ease-in duration-400 hover:text-[#011aff] hover:border-b-2 hover:border-[#011aff] p-2">
